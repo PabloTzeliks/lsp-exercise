d80c564c8784393d42fc39f1245af6cc80093954
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-10 14:53:32 -0300

    feat: add processandor de pagamento

--- src/service/ProcessadorPagamento.java
+++ src/service/ProcessadorPagamento.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.IPedido;
+
 public class ProcessadorPagamento {
+
+    public void calcular(IPedido pedido) {
+
+        double totalPagar = pedido.getValorFinal();
+
+        System.out.println("Total Pagar: " + totalPagar);
+        System.out.println();
+        System.out.println("Processando: " + pedido.getNome());
+    }
 }
